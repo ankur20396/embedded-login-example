8c67337ac8e854b206729fd45e84d164244305f9
Ankur Nigam <ankur20396@gmail.com>
2019-06-19 15:29:55 +0530

    Updated Community URL on line 24 and 25

--- index.php
+++ index.php
@@ -21,8 +21,8 @@
     <meta name="salesforce-self-register-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_ENABLED');?>">
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
-	<link href="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
-    <script src="https://<?php echo getenv('SALESFORCE_COMMUNITY_URL');?>/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
+	<link href="https://ankur-nigam-developer-edition.ap15.force.com/Customers/servlet/servlet.loginwidgetcontroller?type=css" rel="stylesheet" type="text/css" />
+    <script src="https://ankur-nigam-developer-edition.ap15.force.com/Customers/servlet/servlet.loginwidgetcontroller?type=javascript_widget" async defer></script>
   </head>
   
   <body>
